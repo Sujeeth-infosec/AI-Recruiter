b949ce0e151b625f6c19b6ee998c366c1b2a6d22
teswarreddy <thathieswarreddy@gmail.com>
2025-04-10 22:43:19 +0530

    speeling mistake in AppSiderbar.js and provider.js

--- ai-recruiter/app/(main)/_components/AppSiderbar.jsx
+++ ai-recruiter/app/(main)/_components/AppSiderbar.jsx
@@ -5,12 +5,14 @@ import {
     SidebarGroup,
     SidebarHeader,
   } from "@/components/ui/sidebar"
+
+  import logo from "../../../public/logo.png"
   
-  export function AppSidebar() {
+  export function AppSiderbar() {
     return (
       <Sidebar>
         <SidebarHeader className='flex items-center mt-5' />
-        <Image src ="/logo.png" alt="Logo" width={200} height={100} className="w-[150px]"/>
+        <img src ="/logo.png" alt="Logo" width={200} height={100} className="w-[150px]"/>
         <button className='w-full mt-5'><plus/> Create New Interview</button>
         <SidebarContent>
           <SidebarGroup />
--- ai-recruiter/app/(main)/provider.js
+++ ai-recruiter/app/(main)/provider.js
@@ -1,13 +1,12 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import React from "react";
-import { AppSiderbar } from "./_components/AppSiderbar";
+import {AppSiderbar} from "./_components/AppSiderbar";
 
 function DashboardProvider({children}){
     return (
         <SidebarProvider>
             <AppSiderbar/>
         <div>
-            <SidebarTrigger/>
             {children}
         </div>
         </SidebarProvider>
